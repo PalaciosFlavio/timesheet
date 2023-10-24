880a6578a70c8ae362c744ad2b426a4ed52e8cb9
PalaciosFlavio <83726915+PalaciosFlavio@users.noreply.github.com>
2023-10-24 12:04:51 -0300

    Delete proyecto hasta 9-10 directory

--- proyecto hasta 9-10/eliminarReporte.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('db.php');
-
-$cod_recursos=$_POST['txtID'];
-mysqli_query($conexion, "DELETE FROM horas_trabajo WHERE id = '$reporte:id'") or die ("error al eliminar");
-
-mysqli_close($conexion);
-header("location:generar_reporte.php");
-?>
